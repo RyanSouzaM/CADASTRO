e8bfe1ed531c80bb0b38ac247bc14b1760d90527
Ryan Matos <110576228+RyanSouzaM@users.noreply.github.com>
2023-03-02 22:22:26 -0300

    atualização

---
+++ index02.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="continer mt-5 ms-5">
+        <div class="header-title mt-2">
+        <h1>Cadastro novo usuário</h1>
+        </div>
+        
+        <div class="col">
+            <div class="row">
+                <div class="col-md-4 ml-auto">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <input type="text" name="nome" class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="form-group mt-1 ">
+                            <input type="text" name="sobrenome" class="form-control" placeholder="Sobrenome">
+                        </div>
+                        <div class="form-group mt-1">
+                            <input type="email" name="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group mt-1">
+                            <input type="number" name="phone" class="form-control" placeholder="Phone">
+                        </div>
+                        <div class="mt-3">
+                        </div>
+                        <p class="small mt-3">
+                        </p>
+                    </form>
+                </div>
+                <div class="col">
+                    <div class="continer">
+                        <div class="row">
+                            <div class="col-md-6 ml-auto">
+                                <form action="" method="post">
+                                    <div class="form-group">
+                                        <input type="password" name="senha" class="form-control" placeholder="Senha">
+                                    </div>
+                                    <div class="form-group mt-1">
+                                        <input type="password" name="email" class="form-control" placeholder="Confirmar Senha">
+                                    </div>
+                                    <div class="mt-1">
+                                        <select class="form-select">
+                                            <option selected>Selecione uma pergunta de segurança</option>
+                                            <option >Qual estação do ano você nasceu</option>
+                                            <option >Qual numero de telefone antigo</option>
+                                            <option >Qual o nome do seu PET de extimação</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group mt-1">
+                                        <input type="text" name="resposta" class="form-control" placeholder="Resposta">
+                                    </div>
+                                    <div class=" col-md-5 mt-3">
+                                        <button type="submit" class="btn btn-primary w-100">Cadastr-se agora!</button>
+                                    </div>
+                                    <p class="small mt-3">
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index03.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Cadastro</h1>
+    <br>
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Senha</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+    </div>
+    
+</div>
+    
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Endereço</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos mandrake nº 1533">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Endereço 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc.">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Cidade</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputEstado">Estado</label>
+      <select id="inputEstado" class="form-control">
+        <option selected>Escolher...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputCEP">CEP</label>
+      <input type="text" class="form-control" id="inputCEP">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Clique em mim
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+  <div class="form-row align-items-center">
+    <div class="col-auto my-1">
+      <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Sexo</label>
+      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+        <option selected>Escolher...</option>
+        <option value="1">M</option>
+        <option value="2">F</option>
+        <option value="3">Outros</option>
+      </select>
+    </div>
+    <div class="col-auto my-1">
+      <div class="custom-control custom-checkbox mr-sm-2">
+        <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+        <label class="custom-control-label" for="customControlAutosizing">Lembrar preferências</label>
+      </div>
+    </div>
+    <div class="col-auto my-1">
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+  </div>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
